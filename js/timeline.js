17d96cec6186b39ee66d459a848a4254e43f33c0
Riddhi Mistry <32605242+mistryrm@users.noreply.github.com>
2023-08-22 19:41:14 -0400

    uploaded timeline js

---
+++ js/timeline.js
@@ -0,0 +1,7 @@
+/*
+        Due to the nature of the javascript library I am require to host the csv file online
+        However, the same CSV file can be found in /data/riddhi-mistry-art-data-timeline.csv
+      */
+timeline = new TL.Timeline('timeline-embed',
+    'https://docs.google.com/spreadsheets/d/1XQ9bAXj-E_1H6uOhyPhOX5bS8rAmPuxMTEym5Y5KPxM/edit#gid=801854951',
+);
